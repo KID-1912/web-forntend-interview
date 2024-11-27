831aa7106d2fc53ac7763ce46ddc9bb90c7acd57
heyutongxue <heyutongxue@gmail.com>
2024-11-27 19:52:35 +0800

    feat:20241127

--- JavaScript/code/Regx.js
+++ JavaScript/code/Regx.js
@@ -1,13 +1,34 @@
-const url = "https://www.baidu.com/s/ass/a?wd=javascript&a=2#hash";
+const url = "https://www.baidu.com/s/ass/a?wd=javascript&a=2&c=#hash2";
 
 // 匹配path
-// const pathResult = url.match(/(https?:\/\/[^\/]+)(\/[^?]*)/); // 匹配协议和路径
-// const pathResult = url.match(/(https?:\/\/(\w|\.)+)((\/\w+)+)\?.+/); // 匹配协议和路径
+// const pathResult = url.match(/(https?:\/\/[^\/]+)(\/[^?]*)/);
+// const pathResult = url.match(/(https?:\/\/(\w|\.)+)((\/\w+)+)\?.+/);
 // console.log(pathResult[3]); // 输出匹配的路径
 
 // 匹配query部分 ?xxx=xxx&xxx=xx
 // const queryStringResult = url.match(/https?:\/\/[^?]+(\?[\w=&]*)?(#\w+)?/);
 // console.log(queryStringResult, queryStringResult[1]);
 
-// 提取query部分
+// 格式化query
+// const queryResult = url.match(/https:\/\/[^?]+\?([^#]+)(#\w+)?/);
+// if (queryResult && queryResult[1]) {
+//   const queryString = queryResult[1];
+//   const list = queryString.split("&");
+//   const queryObject = list.reduce((prev, cur) => {
+//     const [key, value] = cur.split("=");
+//     prev[key] = value;
+//     return prev;
+//   }, {});
+//   console.log(queryObject);
+// }
+
+// const searchParams = new URL(url).searchParams;
+// const queryObject = {};
+// for (key of searchParams.keys()) {
+//   queryObject[key] = searchParams.get(key);
+// }
+// console.log(queryObject);
+
 // 匹配hash部分
+// const hashResult = url.match(/https:\/\/[^#]+(#\w+)/);
+// console.log(hashResult && hashResult[1]);
